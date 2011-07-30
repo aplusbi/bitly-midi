f2575e4f31d459dfc47b24ea5f5da8499c308205
Niki Yoshiuchi <aplusbi@gmail.com>
2011-07-30 02:28:17 -0400

    Writes output to midi file

--- src/main.ml
+++ src/main.ml
@@ -23,6 +23,23 @@ let play_note st n =
             Portmidi.write_short st Int32.zero (Portmidi.message 0x80 key 0)
     | _ -> ()
 
+let write_note track ttime n =
+    match n with
+    | `Assoc d ->
+            let url = List.assoc "u" d in
+            let ua = List.assoc "a" d in
+            let key = (Hashtbl.hash url) mod 128 in
+            let time = (Hashtbl.hash ua) mod 100 in
+            let ba = Bigarray.Array1.of_array Bigarray.int8_unsigned Bigarray.c_layout [|0x90; key; 100|] in
+            let event = Smf.event_new_from_pointer ba 3 in
+            Smf.track_add_event_seconds track event (ttime + time);
+            Bigarray.Array1.set ba 0 0x80;
+            Bigarray.Array1.set ba 2 0;
+            let event = Smf.event_new_from_pointer ba 3 in
+            Smf.track_add_event_seconds track event (ttime + (2 * time));
+            ttime + (2 * time)
+    | _ -> ttime
+
 type mode = Input | Output
 
 let get_device m =
@@ -42,7 +59,7 @@ let get_device m =
     done;
     Pervasives.read_int ()
 
-let _ = 
+let play _ = 
     Portmidi.init ();
     Portmidi.Time.start 1;
     let d = get_device Output in
@@ -58,3 +75,20 @@ let _ =
         with End_of_file -> exit 0
     done
 
+let write _ = 
+    let smf = Smf.smf_new () in
+    let track = Smf.track_new () in
+    Smf.add_track smf track;
+    let raw_json = open_in "bitly.json" in
+    let time = ref 0 in
+    while true do
+        try
+            let l = input_line raw_json in
+            try
+                let json = from_string l in
+                time := write_note track (!time) json
+            with _ -> ()
+        with End_of_file -> ignore (Smf.save smf "bitly.mid"); exit 0
+    done
+
+let _ = write ()
